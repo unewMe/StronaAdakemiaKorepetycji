1253bc4e92883f8358629f320f7b8885ab97ae96
daniko10 <125457741+daniko10@users.noreply.github.com>
2023-08-31 16:19:02 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Akademia Korepetycji</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Source+Sans+Pro:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet">
+
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <link href="assets/css/variables.css" rel="stylesheet">
+
+  <link href="assets/css/main.css" rel="stylesheet">
+
+  <style>
+    /* Dodany styl do formularza */
+    .form-container {
+      background-color: #fff;
+      border-radius: 10px;
+      padding: 40px;
+      box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
+      width: 100%;
+      max-width: 600px;
+      margin: 0 auto;
+    }
+
+    .form-container h2 {
+      font-size: 32px;
+      margin-bottom: 20px;
+    }
+
+    .form-group {
+      margin-bottom: 20px;
+    }
+
+    .form-group label {
+      font-size: 18px;
+      font-weight: bold;
+      color: #333;
+      display: block;
+      margin-bottom: 10px;
+    }
+
+    .form-group input[type="text"],
+    .form-group input[type="email"] {
+      width: 100%;
+      padding: 10px;
+      border: 1px ridge #ccc;
+      border-radius: 4px;
+      font-size: 16px;
+    }
+
+    .form-group input[type="submit"] {
+      background-color: #ccc;
+      color: #black;
+      border: none;
+      padding: 12px 20px;
+      border-radius: 4px;
+      font-size: 16px;
+      cursor: pointer;
+      transition: background-color 0.3s;
+    }
+
+    .container-choices {
+        background-color: #ffffff;
+        border-radius: 10px;
+        padding: 40px;
+        box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
+        width: 100%;
+        max-width: 400px;
+    }
+
+    .form-group select {
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    font-size: 16px; }
+
+    
+.course-buttons {
+    background-color: #;
+    width: 250px;
+    padding: 10px 20px;
+    
+    border: none;
+    border-radius: 4px;
+    font-size: 16px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+}
+  .form-group button.active {
+    background-color: #ccc;
+  }
+
+  </style>
+
+</head>
+
+<body>
+
+<header id="header" class="header fixed-top" data-scrollto-offset="0">
+    <div class="container-fluid d-flex align-items-center justify-content-between">
+      <pev></pev>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+        <li><a class="nav-link scrollto" href="index.php">APLIKUJ</a></li>
+        <li><a class="nav-link scrollto" href="about.php">O NAS</a></li> 
+        <li><a class="nav-link scrollto" href="about.php">KONTAKT</a></li>         
+        </ul>
+      </nav>
+
+      <a class="nav-link scrollto" href="index.html#about"></a>
+
+    </div>
+  </header><!-- End Header -->
+
+  <section id="hero-fullscreen" class="hero-fullscreen d-flex align-items-center">
+    <div class="container d-flex flex-column align-items-center position-relative" data-aos="zoom-out">
+      <h2>Witamy w Akademii Korepetycji</h2>
+      <p>Wypełnij poniższy formularz, a odezwiemy się do Ciebie</p>
+
+      <div class="form-container">
+        <form action="process_form.php" method="post">
+          <div class="form-group">
+                <center><p>Przychodzisz do nas jako</p></center>
+                <button type="button" class="course-buttons" data-role="UCZEŃ">Uczeń</button>
+                <button type="button" class="course-buttons" data-role="KOREPETYTOR">Korepetytor</button>
+                <input type="hidden" id="selectedRole" name="selectedRole" value="">
+                <br></br>
+            <input type="text" id="imie" name="imie" placeholder="Imię.." required><br></br>
+            <input type="text" id="nazwisko" name="nazwisko" placeholder="Nazwisko.." required><br></br>
+            <input type="email" id="email" name="email" placeholder="JanKowalski@example.com" required><br></br>
+
+            <p>Czym jesteś zainteresowany(na)</p>
+                <div>
+                <button type="button" class="course-buttons" data-school="PODSTAWOWA" >Szkoła podstawowa</button>
+                <button type="button" class="course-buttons" data-school="PONADPODSTAWOWA" >Szkoła ponadpodstawowa</button>
+                <input type="hidden" id="selectedSchool" name="selectedSchool" value="">
+                <br></br>
+                <button type="button" class="course-buttons" data-level="PODSTAWOWY" >Poziom podstawowy</button>
+                <button type="button" class="course-buttons" data-level="ROZSZERZONY" >Poziom rozszerzony</button>
+                <input type="hidden" id="selectedLevel" name="selectedLevel" value="">
+                <br></br>
+                </div>
+                <select id="kursy" name="kursy[]" required>
+                <option value="MATEMATYKA">Matematyka</option>
+                <option value="FIZYKA">Fizyka</option>
+                <option value="CHEMIA">Chemia</option>
+                <option value="ANGIELSKI">Język angielski</option>
+              </select> <br></br>
+            <center><input type="submit" value="Wyślij"></center>
+          </div>
+        </form>
+      </div>
+    </div>
+  </section>
+
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/js/main.js"></script>
+  <script>
+        const roleButtons = document.querySelectorAll(".course-buttons[data-role]");
+        const levelButtons = document.querySelectorAll(".course-buttons[data-level]");
+        const schoolButtons = document.querySelectorAll(".course-buttons[data-school]");
+        const selectedRoleInput = document.querySelector("#selectedRole");
+        const selectedSchoolInput = document.querySelector("#selectedSchool");
+        const selectedLevelInput = document.querySelector("#selectedLevel");
+
+        roleButtons.forEach(button => {
+            button.addEventListener("click", () => {
+                roleButtons.forEach(btn => btn.classList.remove("active"));
+                button.classList.add("active");
+                selectedRoleInput.value = button.getAttribute("data-role");
+            });
+        });
+
+        schoolButtons.forEach(button => {
+            button.addEventListener("click", () => {
+                schoolButtons.forEach(btn => btn.classList.remove("active"));
+                button.classList.add("active");
+                selectedSchoolInput.value = button.getAttribute("data-school");
+            });
+        });
+
+        levelButtons.forEach(button => {
+            button.addEventListener("click", () => {
+                levelButtons.forEach(btn => btn.classList.remove("active"));
+                button.classList.add("active");
+                selectedLevelInput.value = button.getAttribute("data-level");
+            });
+        });
+  </script>
+</body>
+
+</html>
